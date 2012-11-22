50db6b2b0046150f89a9e6e768b00f58bb0a6744
phillips1021 <phillips1021@hotmail.com>
2012-11-21 21:00:47 -0600

    Added unit tests and a ReadMe.txt file.

---
+++ FixedPoint/ReadMe.txt
@@ -0,0 +1,16 @@
+Solution for Art & Logic Programming Challenge Problem 1a
+
+This project provides a class, FixedPoint, that can encode 
+and decode a float value packed into a 32-bit integer with 
+the following structure.
+
+First bit is the sign bit (1 = negative)
+Next 16 bits are for the integer portion of the float value
+Last 15 bits are for the fraction portion of the float value
+
+Read the JavaDoc comments for the FixedPoint class and its 
+methods to understand how to use it.  Also review the 
+unit test in the test class FixedPointTest (src/test/java) 
+for examples of using the FixedPoint class.
+
+
--- FixedPoint/src/test/java/name/brucephillips/model/FixedPointTest.java
+++ FixedPoint/src/test/java/name/brucephillips/model/FixedPointTest.java
@@ -8,27 +8,71 @@ public class FixedPointTest {
 
 	@Test
 	public void testIsFixedPointPackedNegative() {
-		fail("Not yet implemented");
+
+		int negativeFixedPointPacked = 0x80008000;
+		
+		boolean isNegative = FixedPoint.isFixedPointPackedNegative(negativeFixedPointPacked);
+		
+		assertEquals("FixedPoint is not negative but should be.", true, isNegative);
+		
+		int positiveFixedPointPacked = 0x00008000; 
+		
+		boolean isPositive = FixedPoint.isFixedPointPackedNegative(positiveFixedPointPacked);
+		
+		assertEquals("FixedPoint is not positive but should be.", false, isPositive);
 	}
 
 	@Test
 	public void testGetIntegerPortionFromFixedPointPacked() {
-		fail("Not yet implemented");
+		
+		int fixedPointPacked = 0x00327eb8;
+		
+		int integerPortion = FixedPoint.getIntegerPortionFromFixedPointPacked(fixedPointPacked);
+		
+		assertEquals("Integer portion is not 100 but should be.", 100, integerPortion);
+
 	}
 
 	@Test
 	public void testGetFractionPortionFromFixedPointPacked() {
-		fail("Not yet implemented");
+	
+		int fixedPointPacked = 0x00327eb8;
+		
+		float fractionPortion = FixedPoint.getFractionPortionFromFixedPointPacked(fixedPointPacked);
+		
+		assertTrue("Fraction portion is not approximately .99", fractionPortion < .99999 && fractionPortion > .98888);
+		
+		
 	}
 
 	@Test
 	public void testGetFloatValue() {
-		fail("Not yet implemented");
+	
+		int fixedPointPacked = 0x00327eb8;
+		
+		float expectedFloatValue = 100.99f ;
+		
+		FixedPoint fixedPoint = new FixedPoint( fixedPointPacked );
+		
+		float actualFloatValue = fixedPoint.getFloatValue() ;
+		
+		assertTrue("Float value is not approximately " + expectedFloatValue + " but should be.", expectedFloatValue < actualFloatValue + 0.005 && expectedFloatValue > actualFloatValue - 0.0005);
+	
 	}
 
 	@Test
 	public void testGetFixedPointPacked() {
-		fail("Not yet implemented");
+		
+		float floatValue = -2.5f;
+		
+		int expectedFixedPointPacked = 0x80014000;
+		
+		FixedPoint fixedPoint = new FixedPoint( floatValue);
+		
+		int actualFixedPointPacked = fixedPoint.getFixedPointPacked() ;
+		
+		assertEquals("FixedPoint packed value is not " + expectedFixedPointPacked + " but should be", expectedFixedPointPacked, actualFixedPointPacked );
+		
 	}
 
 }
